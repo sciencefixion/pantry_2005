a26b1bf5ee10af9c0a881ab6bd3708bd1604d497
Rion Hutsell <st.gish@gmail.com>
2020-06-03 10:59:47 -0600

    Add unique test for stock_check and define method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -4,4 +4,12 @@ class Pantry
   def initialize
     @stock = {}
   end
+
+  def stock_check(ingredient)
+    if stock[ingredient].nil?
+      stock[ingredient] = 0
+    else
+      stock[ingredient]
+    end 
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -21,14 +21,16 @@ class PantryTest < Minitest::Test
     assert_equal expected, @pantry.stock
   end
 
-  def test_it_can_restock
-    assert_equal 0, @pantry.stock_check(ingredient1)
+  def test_it_can_check_stock
+    assert_equal 0, @pantry.stock_check(@ingredient1)
+  end
 
-    @pantry.restock(ingredient1, 5)
-    @pantry.restock(ingredient1, 10)
-    assert_equal 15, @pantry.stock_check(ingredient1)
+  def test_it_can_restock
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal 15, @pantry.stock_check(@ingredient1)
 
     @pantry.restock(ingredient2, 7)
-    assert_equal 7, @pantry.stock_check(ingredient2)
+    assert_equal 7, @pantry.stock_check(@ingredient2)
   end
 end
