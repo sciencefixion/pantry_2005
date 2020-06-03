207a275fd26d11c6d43dbe382eb0dce71783d9b7
Rion Hutsell <st.gish@gmail.com>
2020-06-03 11:42:15 -0600

    Add add_ingredient method and ingredients attr_reader

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,10 +1,21 @@
 class Recipe
   attr_reader :name,
-              :ingredients_required
+              :ingredients_required,
+              :ingredients
 
   def initialize(name)
     @name = name
     @ingredients_required = {}
+    @ingredients = @ingredients_required.keys
+  end
+
+  def add_ingredient(ingredient, amnt = 0)
+    if @ingredients_required[ingredient].nil?
+      @ingredients_required[ingredient] = 0
+    else
+      @ingredients_required[ingredient]
+    end
+    @ingredients_required[ingredient] += amnt
   end
 
 end
